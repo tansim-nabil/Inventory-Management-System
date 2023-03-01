5d17431858ca1815de46755b6876fd4e595da891
tansim-nabil <tansim.nabil@gmail.com>
2023-03-01 17:18:06 +0600

    Registration Frontend design added

--- InventoryManagementSystem/Models/ViewModel/LoginSignUpViewModel.cs
+++ InventoryManagementSystem/Models/ViewModel/LoginSignUpViewModel.cs
@@ -6,7 +6,7 @@
 
         public string UserName { get; set; }
         public string Email { get; set; }
-        public long Mobile { get; set; }
+        public long PhoneNumber { get; set; }
         public string Password { get; set; }
         public string ConfirmPassword { get; set; }
     }

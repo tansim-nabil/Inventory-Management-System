9c73d5d3a92a50314d2110c09e15fc0992d7e0f1
tansim-nabil <tansim.nabil@gmail.com>
2023-03-01 16:20:57 +0600

    Added Model "User.cs"

---
+++ InventoryManagementSystem/Models/Account/User.cs
@@ -0,0 +1,12 @@
+﻿namespace InventoryManagementSystem.Models.Account
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public long Mobile { get; set; }
+        public string Password { get; set; }
+    }
+}
